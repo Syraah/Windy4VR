130cf3f513db6ca149763b84b4e3ae00d0087949
Syraah <noudav@gmail.com>
2024-12-16 10:47:00 +0100

    Add files via upload

---
+++ pluginConfig.ts
@@ -0,0 +1,17 @@
+import type { ExternalPluginConfig } from '@windy/interfaces';
+
+const config: ExternalPluginConfig = {
+    name: 'Windy4VR',
+    version: '0.1',
+    title: 'Windy4VR',
+    icon: '🛥',
+    description: 'Plugin windy pour visualiser son routage sur Virtual Regatta',
+    author: 'Syraah',
+    repository: 'https://github.com/Syraah/Windy4VR',
+    desktopUI: 'rhpane',
+    mobileUI: 'small',
+    desktopWidth: 200,
+    routerPath: '/Windy4VR',
+};
+
+export default config;
